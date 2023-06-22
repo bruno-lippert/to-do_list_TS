913423e85a15eee34565fb8928afe2c4b519d625
bruno-lippert <brunolippert2002@gmail.com>
2023-06-21 22:11:24 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,35 @@ import ItemList from './components/ListItem'
 import AddArea from './components/AddArea'
 
 export default function App() {
-const [list, setList] = useState<Item[]>([
-  { id: 1, name: 'Ir ao mercado', done: false},
-  { id: 2, name: 'Ir a farmácia', done: false}
-])
- 
-const handleAddTask = (taskName: string) => {
-  const newList = [...list]
-  newList.push({
-    id: list.length +1,
-    name: taskName,
-    done: false
-  });
-  setList(newList)     
-}
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Ir ao mercado', done: false },
+    { id: 2, name: 'Ir a farmácia', done: false }
+  ])
+
+  const handleAddTask = (taskName: string) => {
+    const newList = [...list]
+    
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+
+    setList(newList)
+  }
+
+  const handleRemoveTask = (position: number) => {
+    const newList = [...list]
+    newList.splice(position, 1)
+
+    let newSequence: number = 1
+    newList.forEach((e) => {
+      e.id = newSequence
+      newSequence++;
+    })
+
+    setList(newList)
+  }
 
   return (
     <Style.Container>
@@ -28,7 +43,7 @@ const handleAddTask = (taskName: string) => {
         <AddArea onEnter={handleAddTask} />
 
         {list.map((item, index) => (
-          <ItemList key={index} item={item}/>
+          <ItemList key={index} position={index} item={item} handleRemoveTask={handleRemoveTask} />
         ))}
       </Style.Area>
     </Style.Container>
--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -4,22 +4,24 @@ import * as Style from './styles'
 import { BsFillTrash3Fill  } from 'react-icons/bs';
 
 type Props = {
-    item: Item
+    item: Item,
+    position: number,
+    handleRemoveTask: (key: number) => void
 }
 
-export default function ItemList({ item }: Props) {
+export default function ItemList({ item, position, handleRemoveTask }: Props) {
     const [isChecked, setIsChecked] = useState(item.done)
 
-    const handleClick = () => {
-
+    const handleClick = (key: number) => {
+        handleRemoveTask(key)
     }
 
     return (
         <Style.Container done={isChecked}>
-            <input type="checkbox"  checked={isChecked} onChange={e => setIsChecked(e.target.checked)}/>
-            <label>{item.id} - {item.name} - {item.done.toString()}</label>
+            <input type="checkbox"  checked={isChecked} onChange={e => setIsChecked(e.target.checked)} onClick={() => (isChecked ? item.done = false : item.done = true)}/>
+            <label>{item.id} - {item.name}</label>
             <div className='deleteContainer'>
-                <div className='deleteButton' onClick={handleClick}><BsFillTrash3Fill /></div>
+                <div className='deleteButton' onClick={()=>handleClick(position)}><BsFillTrash3Fill /></div>
             </div>
         </Style.Container>
     )
